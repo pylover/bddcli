73f38e437a3601d1cd780c9fd5a131d80413348b
pylover <vahid.mardani@gmail.com>
2019-02-17 00:31:59 +0330

    Response class

---
+++ bddcli/response.py
@@ -0,0 +1,24 @@
+import re
+
+
+class Response:
+    body = None
+
+    def __init__(self, status, stdout=None, stderr=None):
+        self.status = HTTPStatus(status)
+        self.stdout = stdout
+        self.stderr = stderr
+
+    def to_dict(self):
+        return dict(
+            status=self.status,
+            stdout=self.stdout,
+            stderr=self.stderr
+        )
+
+    def __eq__(self, other: 'Response'):
+        return self.status == other.status \
+                and self.stdout == other.stdout \
+                and self.stdout == other.stdout
+
+
