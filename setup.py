359455447f04705473722b9735e619dfac61d1b7
pylover <vahid.mardani@gmail.com>
2019-02-16 22:41:48 +0330

    setup.py file

---
+++ setup.py
@@ -0,0 +1,42 @@
+import os.path
+import re
+
+from setuptools import setup, find_packages
+
+
+# reading package's version (same way sqlalchemy does)
+with open(
+    os.path.join(os.path.dirname(__file__), 'bddcli', '__init__.py')
+) as v_file:
+    package_version = \
+        re.compile('.*__version__ = \'(.*?)\'', re.S)\
+        .match(v_file.read())\
+        .group(1)
+
+
+setup(
+    name='bddcli',
+    version=package_version,
+    author='Vahid Mardani',
+    author_email='vahid.mardani@gmail.com',
+    url='http://github.com/pylover/bddcli',
+    description='Test any command line interface in BDD manner.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # This is important!
+    install_requires=[
+    ],
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+    ]
+)
+
