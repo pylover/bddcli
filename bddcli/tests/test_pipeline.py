1648a05ae6e1f1641fdf984f9551b82fbef80919
pylover <vahid.mardani@gmail.com>
2019-02-18 01:57:53 +0330

    test stdin

--- bddcli/tests/test_pipeline.py
+++ bddcli/tests/test_pipeline.py
@@ -8,22 +8,24 @@ from bddcli import Command, when, stdout, status, stderr, Application
 def foo():
     stdin = sys.stdin.read()
     if stdin == 'bad':
-        print('Bad', file=sys.stderr)
+        print(f'error: {stdin}', file=sys.stderr)
         return 1
-
-    print('Foo')
-    return 0
+    elif stdin:
+        print(f'input: {stdin}')
 
 
 def test_basic_pipeline():
     app = Application('foo', 'bddcli.tests.test_pipeline:foo')
-    with Command(app, 'Wihtout any parameter'):
-        assert stderr == ''
+    with Command(app, 'Wihtout any parameter and event any pipe.'):
         assert status == 0
-        assert stdout == 'Foo\n'
-
-        when('Standart input is bad', stdin='bad')
-        assert status == 1
-        assert stderr == 'Bad\n'
         assert stdout == ''
+        assert stderr == ''
+
+        when('stdin is given', stdin='bar')
+        assert status == 0
+        assert stdout == f'input: bar\n'
+
+        when('Exit status is not zero', stdin='bad')
+        assert status == 1
+        assert stderr == f'error: bad\n'
 
