60997c9c7d0adc2d02d49e97aff18441affa28f9
pylover <vahid.mardani@gmail.com>
2019-02-22 13:42:01 +0330

    Test list manipulations

--- bddcli/tests/test_manipulators.py
+++ bddcli/tests/test_manipulators.py
@@ -3,22 +3,31 @@ import sys
 
 import pytest
 
-from bddcli import Command, when, stdout, Application, given
+from bddcli import Command, when, stdout, Application, given, stderr
 
 
 def baz():
     e = os.environ.copy()
     del e['PWD']
     print(' '.join(f'{k}: {v}' for k, v in e.items()))
+    print(' '.join(sys.argv), file=sys.stderr)
 
 
 app = Application('foo', 'bddcli.tests.test_manipulators:baz')
 
 
-def test_manipulators():
-    with Command(app, 'Test manipulators', environ={'bar': 'baz'}):
+def test_dict_manipulators():
+    with Command(app, 'Test dictionary manipulators', environ={'bar': 'baz'}):
         assert stdout == 'bar: baz\n'
 
         with pytest.raises(ValueError):
-            when('Without any argument', environ=given + {'bar': 'qux'})
+            when('Append an existing item', environ=given + {'bar': 'qux'})
+
+
+def test_list_manipulators():
+    with Command(app, 'Test list manipulators', positionals=['bar']):
+        assert stderr == 'foo bar\n'
+
+        with pytest.raises(ValueError):
+            when('Append invalid type', positionals=given + {'invalid': 'qux'})
 
