b388d66ca1ce219149599d756e94143134adc9ad
pynuxer <mehdiazizi@outlook.com>
2019-02-25 14:30:45 +0330

    Change positionals to arguments

--- bddcli/tests/test_arguments.py
+++ bddcli/tests/test_arguments.py
@@ -12,22 +12,13 @@ def foo():  # pragma: no cover
 app = Application('foo', 'bddcli.tests.test_arguments:foo')
 
 
-def test_positionals():
-    with Command(app, 'Pass single positional argument', positionals=['bar']):
+def test_arguments():
+    with Command(app, 'Pass single positional argument', arguments=['bar']):
         assert stdout == 'foo bar\n'
 
-        when('Without any argument', positionals=given - 'bar')
+        when('Without any argument', arguments=given - 'bar')
         assert stdout == 'foo\n'
 
-        when('Pass multiple arguments', positionals=given + 'baz')
+        when('Pass multiple arguments', arguments=given + 'baz')
         assert stdout == 'foo bar baz\n'
-
-
-def test_flags():
-    with Command(
-        app,
-        'Pass single optional argument',
-        flags=['--bar=baz']
-    ):
-        assert stdout == 'foo --bar=baz\n'
 
