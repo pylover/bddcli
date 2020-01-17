ac5917badcdd964cd92d2335c8ab313afb47c727
pylover <vahid.mardani@gmail.com>
2020-01-17 13:00:51 +0330

    2.5.1

--- bddcli_bootstrapper/__init__.py
+++ bddcli_bootstrapper/__init__.py
@@ -7,8 +7,11 @@ def main():
     prog = sys.argv.pop(1)
     address = sys.argv.pop(1)
     sys.argv[0] = prog
-
     module, expression = address.split(':')
+
+    if '' not in sys.path:
+        sys.path.insert(0, '')
+
     module = importlib.import_module(module)
     func = eval(f'{expression}', globals(), module.__dict__)
     return func()
